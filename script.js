7039b0c723a660f438f44b286ae7512e91873ae6
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-05-28 14:18:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 //your JS code here. If required.
 const squares = document.querySelectorAll("square")
 
-squares.forEach((square, index) => {
-	square.addEventListener('mouseenter', () => {
-		squares.forEach((other, i) => {
-			if(i !== index) {
-				other.style.backgroundColor = '#6F4E37'
-			}
-		})
-	})
+squares.forEach((square) => {
+      square.addEventListener('mouseenter', () => {
+        squares.forEach((s) => {
+          if (s !== square) {
+            s.style.backgroundColor = '#6F4E37'; // Coffee
+          }
+        });
+      });
 
-	square.addEventListener('mouselevel', () => {
-		squares.forEach(other => {
-			other.style.backgroundColor = '#E6E6FA'
-		})
-	})
-})
+      square.addEventListener('mouseleave', () => {
+        squares.forEach((s) => {
+          s.style.backgroundColor = '#E6E6FA'; // Lavender
+        });
+      });
