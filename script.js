3845bc34fad4bd5e9fe7a700dcb72cb237986d6a
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-05-28 14:48:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 //your JS code here. If required.
- const squares = document.querySelectorAll('.square');
+const squares = document.querySelectorAll('.square');
+const square1 = document.getElementById('square1');
+const square2 = document.getElementById('square2'); 
+const square3 = document.getElementById('square3');
 
-    squares.forEach((square) => {
-      square.addEventListener('mouseenter', () => {
-        squares.forEach((s) => {
-          if (s !== square) {
-            s.style.backgroundColor = '#6F4E37'; 
-          }
-        });
-      });
+const originalColor = square1.style.backgroundColor;
 
-      square.addEventListener('mouseleave', () => {
-        squares.forEach((s) => {
-          s.style.backgroundColor = '#E6E6FA'; 
-        });
-      });
-    });
+squares.forEach(function(sqr) {
+  sqr.addEventListener('mouseover', (e) => {
+    if (e.target.id === 'square1') {
+      square2.style.backgroundColor = '#6F4E37';
+      square3.style.backgroundColor = '#6F4E37';
+    }else if (e.target.id === 'square2') {
+      square1.style.backgroundColor = '#6F4E37';
+      square3.style.backgroundColor = '#6F4E37';
+    } else if (e.target.id === 'square3') {
+      square1.style.backgroundColor = '#6F4E37';
+      square2.style.backgroundColor = '#6F4E37';
+    }
+  });
+
+  sqr.addEventListener('mouseout', (e) => {
+    if (e.target.id === 'square1') {
+      square2.style.backgroundColor = originalColor;
+      square3.style.backgroundColor = originalColor;
+    }else if (e.target.id === 'square2') {
+      square1.style.backgroundColor = originalColor;
+      square3.style.backgroundColor = originalColor;
+    } else if (e.target.id === 'square3') {
+      square1.style.backgroundColor = originalColor;
+      square2.style.backgroundColor = originalColor;
+    }
+  });
+});
